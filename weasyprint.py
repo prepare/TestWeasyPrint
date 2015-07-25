213e34cab4eaa2342d39e2bfaa2dfeaa64c755e2
win <wintercoredev@gmail.com>
2015-07-25 14:30:00 +0700

    02: add test file
    acid1 pass
    acid 2 : errs

--- weasyprint.py
+++ weasyprint.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
+
+# my test startup params notes
+# tests/1_acid2_dev/acid2.htm tests/1_acid2_dev/acid2.png
+# tests/0_acid1_dev/00.ACID1.htm tests/0_acid1_dev/00.ACID1.pdf 
+
+
 from weasyprint.__main__ import main
 
 if __name__ == '__main__':
